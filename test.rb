f17e87668fb738f8c67300ba5c91e4b6f8b91039
Will Larson <lethain@gmail.com>
2017-02-02 07:05:15 -0800

    Compare table content too.

--- test.rb
+++ test.rb
@@ -17,21 +17,11 @@ class HelloWorldTest < Test::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_papers_empty
-    drop_tables(@tables)
-    get '/papers/'
-    assert last_response.ok?
-    doc = Nokogiri::HTML(last_response.body)
-    rows = doc.css("tbody tr")
-    assert_equal 0, rows.size
-  end
-
-  def test_papers_not_empty
+  def test_papers_list
     drop_tables(@tables)
     p = Papers.new(mysql)
     args = ['Name', 'Link', 'Desc', 'Topic', '2017', 'Slug']
-    (1..2).each do |n|
-      p.create(*args)
+    (0..2).each do |n|
       get '/papers/'
       assert last_response.ok?
       doc = Nokogiri::HTML(last_response.body)
@@ -41,6 +31,7 @@ class HelloWorldTest < Test::Unit::TestCase
         cols = row.css("td").map { |x| x.text }
         assert_equal ['Name', '2017', 'Topic', ''], cols
       end
+      p.create(*args)      
     end
   end
 
