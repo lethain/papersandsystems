31f582d465bed701c3c3c960c8c6515c89c00ebf
Will Larson <lethain@gmail.com>
2017-02-02 07:09:35 -0800

    Add test for listing systems

--- test.rb
+++ test.rb
@@ -17,6 +17,24 @@ class HelloWorldTest < Test::Unit::TestCase
     Sinatra::Application
   end
 
+  def test_systems_list
+    drop_tables(@tables)
+    s = Systems.new(mysql)
+    args = ['System 1', 'system_1']
+    (0..2).each do |n|
+      get '/'
+      assert last_response.ok?
+      doc = Nokogiri::HTML(last_response.body)
+      rows = doc.css("tbody tr")
+      assert_equal n, rows.size
+      rows.each do |row|
+        cols = row.css("td").map { |x| x.text }
+        assert_equal ["System 1", "0 engineers", "New"], cols
+      end
+      s.create(*args)
+    end
+  end
+
   def test_papers_list
     drop_tables(@tables)
     p = Papers.new(mysql)
@@ -31,7 +49,7 @@ class HelloWorldTest < Test::Unit::TestCase
         cols = row.css("td").map { |x| x.text }
         assert_equal ['Name', '2017', 'Topic', ''], cols
       end
-      p.create(*args)      
+      p.create(*args)
     end
   end
 
