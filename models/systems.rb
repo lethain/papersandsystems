6deda95531a10d66efebd0653646dc72c2a5faf2
Will Larson <lethain@gmail.com>
2017-02-04 10:20:14 -0800

    Rubocop systems model

--- models/systems.rb
+++ models/systems.rb
@@ -1,7 +1,6 @@
 require 'mysql2'
 require './utils'
 
-
 class Systems < PASModel
   def initialize(mysql)
     super(mysql, 'systems')
@@ -9,18 +8,17 @@ class Systems < PASModel
 
   def get_by_template_or_id(segment)
     if segment.length == 36
-      self.get('id', segment)
+      get('id', segment)
     else
-      self.get('template', segment)
+      get('template', segment)
     end
   end
 
   def create(name, template)
-    id = self.uuid
-    pos = self.count() + 1
-    name, template = [name, template].map { |x| self.escape(x) }
+    id = uuid
+    pos = count + 1
+    name, template = [name, template].map { |x| escape(x) }
     sql = "INSERT INTO #{@table} (id, pos, name, template) VALUES ('#{id}', '#{pos}', '#{name}', '#{template}')"
-    self.run(sql)
+    run(sql)
   end
-  
 end
