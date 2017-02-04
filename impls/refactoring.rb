7d0360e9b03579dc7c87204c34c1f6163d2f0e59
Will Larson <lethain@gmail.com>
2017-02-04 15:11:55 -0800

    Finish implementing refactoring.

--- impls/refactoring.rb
+++ impls/refactoring.rb
@@ -16,16 +16,26 @@ def parse(txt)
 end
 
 def rewrite(expr)
-  if (expr.is_a? Sexp) && expr[0] == :call && expr[2] == :incr
-    num_args = expr.slice(3, expr.size).size
-    if num_args == 2
-      snd = expr[4]
-      if snd[0] == :lit && snd[1] == 1
-        # remove the second value!
-        expr.pop()
+  if expr.is_a? Sexp
+    if expr[0] == :call
+      num_args = expr.slice(3, expr.size).size
+      if num_args == 2
+        snd = expr[4]
+        if snd[0] == :lit && snd[1] == 1
+          # remove the second value!
+          expr.pop()
+        end
       end
+    elsif expr[0] == :for
+      lst = expr[1]
+      param = expr[2]
+      func = expr[3]
+      expr.clear
+      expr[0] = :iter
+      expr[1] = Sexp.new(:call, lst, :each)
+      expr[2] = Sexp.new(:args, param[1])
+      expr[3] = func
     end
-  elsif expr.is_a? Sexp
     expr.each do |x|
       rewrite(x)
     end
@@ -33,5 +43,14 @@ def rewrite(expr)
   expr
 end
 
-
-puts Ruby2Ruby.new.process rewrite(parse(load))
+rewritten = Ruby2Ruby.new.process rewrite(parse(load))
+regex = /^(\s+)\((.*)\)\s*$/
+rewritten = rewritten.split("\n").map do |line|
+  if line =~ regex
+    match = regex.match(line)    
+    match[1] + match[2]
+  else    
+    line
+  end
+end
+puts rewritten
