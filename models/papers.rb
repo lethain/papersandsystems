48d64c9d32ff136887b12041e3a0b160497c56a1
Will Larson <lethain@gmail.com>
2017-02-04 10:16:30 -0800

    Rubocop papers model

--- models/papers.rb
+++ models/papers.rb
@@ -1,17 +1,16 @@
 require 'mysql2'
 require './utils'
 
-
 class Papers < PASModel
   def initialize(mysql)
     super(mysql, 'papers')
   end
 
   def create(name, link, description, topic, year, slug)
-    id = self.uuid
-    pos = self.count() + 1    
-    name, link, description, topic, year, slug = [name, link, description, topic, year, slug].map { |x| self.escape(x) }
+    id = uuid
+    pos = count + 1
+    name, link, description, topic, year, slug = [name, link, description, topic, year, slug].map { |x| escape(x) }
     sql = "INSERT INTO #{@table} (id, pos, name, link, description, topic, year, slug) VALUES ('#{id}', '#{pos}', '#{name}', '#{link}', '#{description}', '#{topic}', '#{year}', '#{slug}')"
-    self.run(sql)
+    run(sql)
   end
 end
