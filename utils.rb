de9599af43556b8c92b02b95c745ea2d2ccb5b91
Will Larson <lethain@gmail.com>
2017-02-04 10:37:11 -0800

    Rubocop on utils

--- utils.rb
+++ utils.rb
@@ -5,19 +5,16 @@ require 'base64'
 require 'cgi'
 require 'securerandom'
 
-UPLOAD_TOKEN_KEY = 'ad5b5ecb30391fb29294891f6b5e4cf6685fae26'
-UPLOAD_TOKEN_IV = "yz\xF5\xCB\x0E\xB8\xE7\xD5'\x80h\x85Q\xCD)\x18"
-
+UPLOAD_TOKEN_KEY = 'ad5b5ecb30391fb29294891f6b5e4cf6685fae26'.freeze
+UPLOAD_TOKEN_IV = "yz\xF5\xCB\x0E\xB8\xE7\xD5'\x80h\x85Q\xCD)\x18".freeze
 
 # monkey-patch Mysql2::Result to have empty? method to
 # avoid rubocop rewrites breaking things
 class Mysql2::Result
   def empty?
-     size == 0
+    size == 0
   end
 end
-
-
 
 def get_logger
   Logger.new(STDERR)
@@ -26,17 +23,15 @@ end
 def extract_desc(s)
   acc = ''
   s.split("\n").each do |line|
-    if line.strip == '' or acc.length > 300
-      break
-    end
-    acc += line + " "
+    break if (line.strip == '') || acc.length > 300
+    acc += line + ' '
   end
-  acc.strip.gsub(/\[(.*)\]\(.*\)/, '\1').gsub(/\*/, '')
+  acc.strip.gsub(/\[(.*)\]\(.*\)/, '\1').delete('*')
 end
 
 def upload_token(uid)
   ts = Time.now.to_i
-  cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+  cipher = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
   cipher.encrypt
   cipher.key = UPLOAD_TOKEN_KEY
   cipher.iv = UPLOAD_TOKEN_IV
@@ -47,13 +42,13 @@ end
 
 def decode_token(token)
   token = Base64.urlsafe_decode64(CGI.unescape(token))
-  cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+  cipher = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
   cipher.decrypt
   cipher.key = UPLOAD_TOKEN_KEY
   cipher.iv = UPLOAD_TOKEN_IV
   decrypted = cipher.update(token)
   decrypted << cipher.final
-  decrypted.split(",")
+  decrypted.split(',')
 end
 
 class PASModel
@@ -67,65 +62,63 @@ class PASModel
     begin
       results = @m.query(sql)
     ensure
-      self.log(sql, results)
+      log(sql, results)
     end
     results
   end
 
-  def get(col, val, fields=nil)
+  def get(col, val, fields = nil)
     if val
-      val = self.escape(val)
+      val = escape(val)
       fields = fields ? fields.join(', ') : '*'
       sql = "SELECT #{fields} FROM #{@table} WHERE #{col}='#{val}' LIMIT 1"
-      results = self.run(sql)
-      results.size > 0 ? results.first : nil
+      results = run(sql)
+      !results.empty? ? results.first : nil
     end
   end
 
-  def list(opts=nil)
+  def list(opts = nil)
     opts = opts ? opts : {}
     sort = opts[:sort] ? opts[:sort] : 'id'
-    cols = opts[:cols] ? opts[:cols].join(", ") : '*'
+    cols = opts[:cols] ? opts[:cols].join(', ') : '*'
     sql = "SELECT #{cols} FROM #{@table}"
     if opts[:where]
-      filters = opts[:where].map do |k,v|
-        "#{k}='#{self.escape(v)}'"
+      filters = opts[:where].map do |k, v|
+        "#{k}='#{escape(v)}'"
       end
       sql += " WHERE #{filters.join(' and ')}"
     end
     sql += " ORDER BY #{sort}"
-    if opts[:limit]
-      sql += " LIMIT #{opts[:limit]}"
-    end
-    self.run(sql)
+    sql += " LIMIT #{opts[:limit]}" if opts[:limit]
+    run(sql)
   end
 
   def update(id, kvs)
     sql = "UPDATE #{@table} SET "
-    updates = kvs.map { |k, v| "#{k}='#{self.escape(v)}'" }
-    sql += updates.join(", ")
-    sql += " WHERE id='#{self.escape(id)}'"
-    self.run(sql)
+    updates = kvs.map { |k, v| "#{k}='#{escape(v)}'" }
+    sql += updates.join(', ')
+    sql += " WHERE id='#{escape(id)}'"
+    run(sql)
   end
 
   def incr(id, key)
-    sql = "UPDATE #{@table} SET #{key} = #{key} + 1 WHERE id='#{self.escape(id)}'"
-    self.run(sql)
+    sql = "UPDATE #{@table} SET #{key} = #{key} + 1 WHERE id='#{escape(id)}'"
+    run(sql)
   end
 
-  def count(opts=nil)
+  def count(opts = nil)
     opts = opts ? opts : {}
     key = 'COUNT(id)'
     sql = "SELECT #{key} FROM #{@table}"
     if opts[:where]
-      filters = opts[:where].map do |k,v|
-        "#{k}='#{self.escape(v)}'"
+      filters = opts[:where].map do |k, v|
+        "#{k}='#{escape(v)}'"
       end
       sql += " WHERE #{filters.join(' and ')}"
     end
 
-    res = self.run(sql).to_a
-    if res.size and res[0].has_key?(key)
+    res = run(sql).to_a
+    if res.size && res[0].key?(key)
       res[0][key]
     else
       0
@@ -141,11 +134,14 @@ class PASModel
   end
 
   def escape(val)
-    (val.is_a? String) ? @m.escape(val) : val
+    if val.is_a? String
+      @m.escape(val)
+    else
+      val
+    end
   end
 
   def standard(s)
     @m.escape(s.gsub(/\W+/, ' ').strip)
   end
-
 end
