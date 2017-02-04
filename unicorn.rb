424b655766c56aa0ff19552bf2b115ac00fd5359
Will Larson <lethain@gmail.com>
2017-02-04 09:47:35 -0800

    Fix unicorn.rb

--- unicorn.rb
+++ unicorn.rb
@@ -1,15 +1,15 @@
 # Set the working application directory
-working_directory "/home/papers/papersandsystems"
+working_directory '/home/papers/papersandsystems'
 
 # Unicorn PID file location
-pid "/home/papers/unicorn.pid"
+pid '/home/papers/unicorn.pid'
 
 # Path to logs
-stderr_path "/home/papers/papers_unicorn.out"
-stdout_path "/home/papers/papers_unicorn.err"
+stderr_path '/home/papers/papers_unicorn.out'
+stdout_path '/home/papers/papers_unicorn.err'
 
 # Unicorn socket
-listen "127.0.0.1:9292"
+listen '127.0.0.1:9292'
 
 # Number of processes
 worker_processes 4
